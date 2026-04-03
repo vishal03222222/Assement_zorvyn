f3c25f1fed58c4d6fbc647e66c310fa60801f9bd
Vishal <mgvishalctr@gmail.com>
2026-04-03 17:16:05 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: true,
-   
+    host: "::",
+    port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
